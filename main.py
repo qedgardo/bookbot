daf9c21cdc4dd6bbd91ba05e3c3de904c53cc47d
Edgardo Quiroga <edgardo@xlabs.xyz>
2025-05-29 17:46:07 -0300

    get book words function added

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def get_book_text(path_to_book):
     with open(path_to_book) as f:
         return f.read()
 
+def get_book_words(book_text):
+    return len(book_text.split())
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print(book_text)
+    num_words = get_book_words(book_text)
+    print(f"{num_words} words found in the document")
 
 main()
